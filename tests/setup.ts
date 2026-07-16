9fb170e8b250fda96a25565af7e41f12bbd8d71e
jmal <zhushilun084@gmail.com>
2026-07-16 17:09:24 +0800

    test: 增加前端单元测试环境

---
+++ tests/setup.ts
@@ -0,0 +1,11 @@
+import { afterEach, beforeEach } from 'vitest'
+
+beforeEach(() => {
+  window.localStorage.clear()
+  window.sessionStorage.clear()
+})
+
+afterEach(() => {
+  window.localStorage.clear()
+  window.sessionStorage.clear()
+})
